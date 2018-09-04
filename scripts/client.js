f0ced794f49c2c1a9bf423077f193365dd3d0c4f
Joseph Carey <josephcareycomposer@gmail.com>
2018-09-04 14:25:33 -0500

    added comments and cleaned up code

--- scripts/client.js
+++ scripts/client.js
@@ -1,7 +1,7 @@
 console.log( 'js' );
 
+// to keep track of how many times we've added a div
 addCounter = 1;
-
 
 
 $( 'document' ).ready( readyNow );
@@ -9,6 +9,7 @@ $( 'document' ).ready( readyNow );
 function readyNow() {
     console.log( 'jQ' );
 
+    // Add listeners/handlers
     $( '#generateButton' ).on( 'click', handleGenerateButton );
     $( '#generatedDiv' ).on( 'click', '.deleteMeButton', handleDeleteMeButton )
     $( '#generatedDiv' ).on( 'click', '.swapColorButton', handleSwapColorButton )
@@ -17,6 +18,7 @@ function readyNow() {
 
 function handleGenerateButton() {
 
+    // template for new Divs
     newDivTemplate = `
     <div class="container col-12 red">
         <p>${addCounter}</p>
@@ -26,8 +28,11 @@ function handleGenerateButton() {
     `
 
     // do we actually need to append each button separately? That's how the assignment is describing it.
+
+    // append the whole kit and kaboodle
     $( '#generatedDiv' ).append( newDivTemplate );
 
+    // increment addCounter
     addCounter++;
 
 }
@@ -41,10 +46,10 @@ function handleDeleteMeButton() {
 
 function handleSwapColorButton() {
 
-    console.log( 'in handleSwapColorButton' );
+    // toggleClass checks if a class is already on an object;
+    // if yes, it takes it away; if no it adds it.
+    // so, we call this on both the red class and the yellow class,
+    // only one is ever present, so it effectively toggles it.
     $( this.closest( 'div' ) ).toggleClass( 'red yellow' );
-    // parentDiv = this.closest( 'div' );
-    // parentDiv.toggleClass( 'red' );
-
 
 }
